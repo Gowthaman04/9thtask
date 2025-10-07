74069f839ad9cf024000b406bfde83e405431427
Gowthaman04 <gowthamsuresh004@gmail.com>
2025-10-07 16:01:02 +0530

    Add Terraform, k8s, and workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 provider "google" {
-  credentials = file(var.gcp_credentials_file)
+  credentials = var.gcp_credentials
   project     = var.project_id
-  region      = var.region
+  region      = "us-central1"
 }
+
 
 
 resource "google_container_cluster" "gke_cluster" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "msd-15051998" {}
 variable "region" { default = "us-central1" }
-variable "MSD_15051998" {}
+variable "GCP_CREDENTIALS_JSON" {}
